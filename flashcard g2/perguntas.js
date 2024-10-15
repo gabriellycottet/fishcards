4a401b57eb4d1f97831d7ec9e3d32023d6c80d8d
gabriellycottet <142987467+gabriellycottet@users.noreply.github.com>
2024-10-15 07:24:19 -0300

    Add files via upload

---
+++ flashcard g2/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quantos anos a Primeira Guerra Mundial durou?',
+    '4 anos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália?',
+    'A capital da Austrália é Camberra'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual ator faz o Homem-Aranha atualmente?',
+    'Tom Holland'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz sinfonia em Inglês?',
+    'Oi em ingles é Simphony (sinfoni)'
+)
